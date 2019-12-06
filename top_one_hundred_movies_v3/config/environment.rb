d7587d8b042dabc18588fd424507ebb39bd6eeb4
David Strauch <50710431+dmaster18@users.noreply.github.com>
2019-12-06 11:12:47 -0500

    Add files via upload

---
+++ top_one_hundred_movies_v3/config/environment.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'action_view'
+
+require_relative '../lib/top_one_hundred_movies/scraper'
+require_relative '../lib/top_one_hundred_movies/movie'
+require_relative '../lib/top_one_hundred_movies/cli'
